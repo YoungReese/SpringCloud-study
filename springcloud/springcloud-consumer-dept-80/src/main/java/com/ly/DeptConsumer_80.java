387f0d863267417b3f2b0be41b7f196bd61b1f55
liyang <liyang.hlw@vivo.com>
2021-03-02 10:32:09 +0800

    解决删除.idea

--- springcloud/springcloud-consumer-dept-80/src/main/java/com/ly/DeptConsumer_80.java
+++ springcloud/springcloud-consumer-dept-80/src/main/java/com/ly/DeptConsumer_80.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DeptConsumer_80 {
+
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer_80.class,args);
     }
+
 }
