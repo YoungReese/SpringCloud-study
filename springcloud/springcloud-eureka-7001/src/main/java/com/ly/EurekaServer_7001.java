d35474ba1fff2c7d181c8837e110ccae01185037
liyang <liyang.hlw@vivo.com>
2021-03-03 17:37:18 +0800

    解决获取不到注册进来为服务信息的bug（因该是idea自带的bug，通过maven clean 和 install 解决）

--- springcloud/springcloud-eureka-7001/src/main/java/com/ly/EurekaServer_7001.java
+++ springcloud/springcloud-eureka-7001/src/main/java/com/ly/EurekaServer_7001.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer  // Eureka服务端启动类
+@EnableEurekaServer  // Eureka服务端（服务注册中心）启动类
 public class EurekaServer_7001 {
 
     public static void main(String[] args) {
--- springcloud/springcloud-provider-dept-8001/src/main/java/com/ly/controller/DeptController.java
+++ springcloud/springcloud-provider-dept-8001/src/main/java/com/ly/controller/DeptController.java
@@ -2,7 +2,8 @@ package com.ly.controller;
 
 import com.ly.pojo.Dept;
 import com.ly.service.DeptService;
-import com.ly.service.DeptServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -13,17 +14,19 @@ import java.util.List;
 @RestController
 public class DeptController {
 
+    private static Logger logger = LoggerFactory.getLogger("DeptController");
+
     @GetMapping("/")
     public String hello() {
-        return "hello, it's ok !";
+        logger.info("hello, it's ok!");
+        System.out.println("hello, it's ok!");
+        return "hello, it's ok!";
     }
 
     @Autowired
     private DeptService deptService;
 
-//    //获取一些配置的信息，得到具体的微服务！
-//    @Autowired
-//    private DiscoveryClient client;
+
 
     @PostMapping("/dept/add")
     public boolean addDept(@RequestBody Dept dept) { //@RequestBody
@@ -42,29 +45,38 @@ public class DeptController {
 
     @GetMapping("/dept/list")
     public List<Dept> queryAll() {
+        logger.info("/dept/list");
+        System.out.println("/dept/list");
         return deptService.queryAll();
     }
 
-//    //注册进来的微服务~，获取一些消息~
-//    @GetMapping("/dept/discovery")
-//    public Object discovery() {
-//        //获取微服务列表的清单
-//        List<String> services = client.getServices();
-//        System.out.println("discovery=>services:" + services);
-//
-//        //得到一个具体的微服务信息,通过具体的微服务id，applicationName；
-//        List<ServiceInstance> instances = client.getInstances("SPRINGCLOUD-PROVIDER-DEPT");
-//
-//        for (ServiceInstance instance : instances) {
-//            System.out.println(
-//                    instance.getHost() + "\t" +
-//                            instance.getPort() + "\t" +
-//                            instance.getUri() + "\t" +
-//                            instance.getServiceId()
-//
-//            );
-//        }
-//        return this.client;
-//    }
+
+    // 获取一些配置的信息，得到具体的微服务！
+    @Autowired
+    private DiscoveryClient client;
+
+    // 注册进来的微服务~，获取一些消息~
+    @GetMapping("/dept/discovery")
+    public Object discovery() {
+        logger.info("/dept/discovery");
+        // 获取微服务列表的清单
+
+        List<String> services = client.getServices();
+        System.out.println("discovery=>services:" + services);
+
+        // 得到一个具体的微服务信息,通过具体的微服务id，applicationName
+        List<ServiceInstance> instances = client.getInstances("SPRINGCLOUD-PROVIDER-DEPT-8001");
+
+        for (ServiceInstance instance : instances) {
+            System.out.println(
+                    instance.getHost() + "\t" +
+                            instance.getPort() + "\t" +
+                            instance.getUri() + "\t" +
+                            instance.getServiceId()
+            );
+        }
+
+        return this.client;
+    }
 
 }
